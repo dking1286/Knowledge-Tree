8feb34aecbe70b19bdbfc0849a96028209a5b651
Daniel King <daniel.oliver.king@gmail.com>
2016-01-30 18:00:05 -0800

    Wrote generator functions to streamline constants

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from constants import *
+import constants
 from financial_tools import time_until_zero_balance, decimal_range
 import database
 
@@ -13,29 +13,25 @@ class Model(object):
             to the interest_rate_slider in the Controller instance.
         initial_balance (int): The current initial balance being displayed. This value will
             be matched to the initial_balance_slider in the Controller instance.
-        payoff_times (dict<interest rate, list of points>): A dictionary that contains
-            the data to be displayed. The keys are interest rates in decimal form, and the values
-            are lists of points to be graphed. Each point's x value is a monthly payment,
-            and the y value is the corresponding time to payoff, in years.
+        payoff_times (dict<initial balance, dict<interest rate, dict<payment, payoff time>>>):
+            A nested dictionary structure containing the data to be plotted.
             
     Public methods:
         Model(main=None)
-        
-    Private methods:
-        _calculate_payoff_times()
+        calculate_payoff_times()
     """
     def __init__(self, main=None, db=None):    
         self.main = main
         self.database = db
-        self.interest_rate = DEFAULT_INTEREST_RATE
-        self.initial_balance = DEFAULT_INITIAL_BALANCE
+        self.interest_rate = constants.DEFAULT_INTEREST_RATE
+        self.initial_balance = constants.DEFAULT_INITIAL_BALANCE
         self.payoff_times = dict()
     
     def calculate_payoff_times(self):
         with self.database.transaction():
-            for Bo in range(MINIMUM_INITIAL_BALANCE, MAXIMUM_INITIAL_BALANCE, INITIAL_BALANCE_STEP):
-                for r in decimal_range(MINIMUM_INTEREST_RATE, MAXIMUM_INTEREST_RATE, INTEREST_RATE_STEP):
-                    for p in range(MINIMUM_MONTHLY_PAYMENT, MAXIMUM_MONTHLY_PAYMENT, MONTHLY_PAYMENT_STEP):
+            for Bo in constants.initial_balance_range():
+                for r in constants.interest_rate_range():
+                    for p in constants.monthly_payment_range():
                         print("Calculating for initial balance {0}, rate {1}, monthly payment {2}".format(Bo, r, p))
                         result = time_until_zero_balance(r, Bo, p)
                         if result is not None:
@@ -53,5 +49,8 @@ class Model(object):
                 self.payoff_times[Bo] = self.payoff_times.get(Bo, {})
                 self.payoff_times[Bo][r] = self.payoff_times[Bo].get(r, {})
                 self.payoff_times[Bo][r][p] = t
+    
+    def get_payoff_time(self, Bo=0, r=0, p=0):
+        return self.payoff_times[Bo][r][p]
 
             
