41dbc18b71eff2015c5bc7e6791e9b6039708a4c
Daniel King <daniel.oliver.king@gmail.com>
2016-02-20 17:31:12 -0800

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'Knowledge Tree',
+    'version': '0.1',
+    'author': 'Daniel Oliver King',
+    'author_email': 'daniel.oliver.king@gmail.com',
+    'packages': ['knowledge_tree'],
+    'install_requires': ['nose'],
+    'scripts': [],
+    'description': 'Interactive program that shows how long it will take to pay off a loan'
+}
+
+setup(**config)
