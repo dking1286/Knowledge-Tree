c9e2a7c909d5a47bd021fab8a8c89bb9fa8c754a
Daniel King <daniel.oliver.king@gmail.com>
2016-02-20 18:22:10 -0800

    Changed the format to make PyCharm happy

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'name': 'Knowledge Tree',
-    'version': '0.1',
-    'author': 'Daniel Oliver King',
-    'author_email': 'daniel.oliver.king@gmail.com',
-    'packages': ['knowledge_tree'],
-    'install_requires': ['nose', 'peewee'],
-    'scripts': [],
-    'description': 'Interactive program that shows how long it will take to pay off a loan'
-}
-
-setup(**config)
+setup(
+    name='Knowledge Tree',
+    version='0.1',
+    author='Daniel Oliver King',
+    author_email='daniel.oliver.king@gmail.com',
+    packages=['knowledge_tree'],
+    requires=['nose', 'peewee'],
+    scripts=[],
+    description='Interactive program that shows how long it will take to pay off a loan'
+)
